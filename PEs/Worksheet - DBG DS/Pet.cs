7e042710fb5dd5c58bb25ec438d7e0d223baed75
Evan Kreg <emk4456@g.rit.edu>
2026-01-21 14:01:13 -0500

    couldnt make the nuGet work

---
+++ PEs/Worksheet - DBG DS/Pet.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Worksheet___DBG_DS
+{
+    internal class Pet
+    {
+        private string name;
+        private double weight;
+
+        // Default constructor
+        public Pet()
+        {
+        }
+
+        // Parameterized constructor
+        public Pet(string name, double weight)
+        {
+            this.name = name;
+            this.weight = weight;
+        }
+    }
+}
---
+++ PEs/Worksheet - DBG DS/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Worksheet___DBG_DS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pet[] pets2 = new Pet[5];
+            pets2[0] = new Pet();
+            pets2[1] = new Pet("Lacy", 7.5);
+            
+
+        }
+    }
+}
